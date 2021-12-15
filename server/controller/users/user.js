663c893d0e3a8a9d5b67690b10ac44f20d874e58
namsupark <skatn7979@gmail.com>
2021-12-15 10:53:01 +0900

    Backend-user file route change

--- server/controller/users/user.js
+++ server/controller/users/user.js
@@ -1,4 +1,7 @@
-const User = require("../models/user/user")();
+
+const { DataTypes } = require('sequelize');
+const db = require("../models/index");
+const User = require("../models/user/user")(db.sequelize,DataTypes);
 
 module.exports = {
   login(req, res) {
