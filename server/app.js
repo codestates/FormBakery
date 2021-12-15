68fb2420fd4e38ec98dcecc9953ff72d68205ec2
namsupark <skatn7979@gmail.com>
2021-12-15 17:51:59 +0900

    sub

--- server/app.js
+++ server/app.js
@@ -2,13 +2,13 @@ const express = require("express");
 var cors = require("cors");
 const app = express();
 const port = 80;
-//const userRouter = require('./routers/user');
+const userRouter = require('./routers/user');
 const answerRouter = require('./routers/answer');
 
 app.use(express.json());
 app.use(express.text());
 app.use(cors());
-//app.use('/user',userRouter);
+app.use('/user',userRouter);
 app.use('/answer',answerRouter);
 
 app.get("/", (req, res) => {
