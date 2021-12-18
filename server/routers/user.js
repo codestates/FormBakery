9de396040d9a337f78708d86ea5d4bcacec598a4
JeongSubi <jsb97613@naver.com>
2021-12-18 14:54:17 +0900

    Backend user picture upload feature

--- server/routers/user.js
+++ server/routers/user.js
@@ -2,6 +2,33 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/users/user");
 
+const multer = require("multer");
+const method = require("../method/custom");
+
+const DIR_NAME = __dirname
+  .split("/")
+  .slice(0, __dirname.split("/").length - 1)
+  .join("/");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR_NAME + "/image/user/"); // 파일 업로드 경로
+  },
+  filename: (req, file, cb) => {
+    cb(null, method.randomString(8, file.originalname)); //파일 이름 설정
+  },
+});
+
+const upload = multer({
+  storage,
+});
+
+router.put(
+  "/uploadProfileImage/:email",
+  upload.single("file"),
+  userController.uploadProfileImage
+);
+
 router.post("/login/:email", userController.login);
 router.post("/logout", userController.logout);
 router.post("/githubCallback", userController.githubCallback);
