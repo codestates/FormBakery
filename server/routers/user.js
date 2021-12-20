edc5a93d4dd3b3e74d4a866a3e04f296b3902359
JeongSubi <jsb97613@naver.com>
2021-12-20 12:35:15 +0900

    Backend user feature

--- server/routers/user.js
+++ server/routers/user.js
@@ -34,13 +34,13 @@ router.post("/logout", userController.logout);
 router.post("/githubCallback", userController.githubCallback);
 router.post("/kakaoCallback", userController.kakaoCallback);
 
-router.put("/signup", userController.signup);
+router.post("/signup", userController.signup);
 router.post("/signupEmailAuth", userController.signupEmailAuth);
 router.post("/passwordEmailAuth", userController.passwordEmailAuth);
 
 router.post("/signout/:email", userController.signout);
 
-router.post("/getUserInfo", userController.getUserInfo);
+router.get("/getUserInfo", userController.getUserInfo);
 router.post("/accessTokenRequest", userController.accessTokenRequest);
 router.put("/updateUserInfo/:email", userController.updateUserInfo);
 router.put("/changePassword/:email", userController.changePassword);
